af7afcce8f51e361c8bf4ef446dd8aca87495971
Md Hasib Askari <mdhsbaskari@gmail.com>
2022-06-07 00:55:33 +0600

    Butterfly Pattern (n=4)

--- src/ButterFlyPattern.java
+++ src/ButterFlyPattern.java
@@ -1,15 +1,15 @@
 public class ButterFlyPattern {
     public static void main(String[] args){
-        int n = 8;
-        for (int i = 1; i <= n; i++){
+        int n = 4;
+        for (int i = 1; i <= 2*n; i++){
 //          *** Upper Part ***
-            if (i <= n/2){
+            if (i <= n){
                 // Left '*'
                 for (int j = 1; j<=i; j++){
                     System.out.print("*");
                 }
                 //  Middle Space
-                for (int j = n-2*i; j >= 1; j--){
+                for (int j = 2*n-2*i; j >= 1; j--){
                     System.out.print(" ");
                 }
                 // Right '*'
@@ -19,15 +19,15 @@ public class ButterFlyPattern {
 //          *** Lower Part ***
             } else {
                 // Left '*'
-                for (int j = n-i+1; j>=1; j--){
+                for (int j = 2*n-i+1; j>=1; j--){
                     System.out.print("*");
                 }
                 // Middle Space
-                for (int j = 1; j <= 2*i-n-2; j++){
+                for (int j = 1; j <= 2*i-2*n-2; j++){
                     System.out.print(" ");
                 }
                 // Right '*'
-                for (int j = 1; j<=n-i+1; j++){
+                for (int j = 1; j<=2*n-i+1; j++){
                     System.out.print("*");
                 }
             }
